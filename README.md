735b5acda549f0f7470b89dc12fdba681a26b8af
rsidh <roohanjyot.s.sidhu@aexp.com>
2023-01-20 12:18:38 -0800

    initial set up

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DockQL
+A POC based on a application using Kotlin on two docker containers. The first container will run the server while the second container runs the DBMS
---
+++ src/main/kotlin/com/example/dockql/DockQlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dockql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DockQlApplication
+
+fun main(args: Array<String>) {
+    runApplication<DockQlApplication>(*args)
+}
